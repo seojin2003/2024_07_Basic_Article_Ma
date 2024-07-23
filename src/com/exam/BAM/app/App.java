75d2171de3dc3e94eb50c7e0ceb5b5fc039fbb9b
seojin2003 <seojin20031010@gmail.com>
2024-07-23 21:43:11 +0900

    controller

--- src/com/exam/BAM/app/App.java
+++ src/com/exam/BAM/app/App.java
@@ -1,5 +1,75 @@
 package com.exam.BAM.app;
+import java.util.Scanner;
+
+import com.exam.BAM.controller.ArticleController;
+import com.exam.BAM.controller.Controller;
+import com.exam.BAM.controller.MemberController;
 
 public class App {
+	public void run() {
+		System.out.println("== 프로그램 시작 ==");
+		
+		Scanner sc = new Scanner(System.in);
 
+		MemberController memberController = new MemberController(sc);
+		memberController.makeTestData();
+		ArticleController articleController = new ArticleController(sc);
+
+		memberController.makeTestData();
+		articleController.makeTestData();
+
+		while (true) {
+			System.out.printf("명령어) ");
+			
+			String cmd = sc.nextLine().trim();
+			
+			if (cmd.equals("exit")) {
+				break;
+			} 
+			
+			if (cmd.length() == 0) {
+				System.out.println("명령어를 입력해주세요");
+				continue;
+			}
+
+			if (cmd.equals("member join")) {
+				memberController.doJoin();
+			} else if (cmd.equals("article write")) {
+				articleController.doWrite();
+			} else if (cmd.startsWith("article list")) {
+				articleController.showList(cmd);
+			} else if (cmd.startsWith("article detail ")) {
+				articleController.showDetail(cmd);
+			} else if (cmd.startsWith("article modify ")) {
+				articleController.doModify(cmd);
+			} else if (cmd.startsWith("article delete ")) {
+				articleController.doDelete(cmd);
+			String[] cmdBits = cmd.split(" ");
+
+			if (cmdBits.length < 2) {
+				System.out.println("존재하지 않는 명령어 입니다");
+				continue;
+			}
+
+			String controllerName = cmdBits[0];
+			String methodName = cmdBits[1];
+
+			Controller controller = null;
+
+			if (controllerName.equals("member")) {
+				controller = memberController;
+			} else if (controllerName.equals("article")) {
+				controller = articleController;
+			} else {
+				System.out.println("존재하지 않는 명령어 입니다");
+				continue;
+			}
+
+			controller.doAction(cmd, methodName);
+		}
+
+		sc.close();
+		
+		System.out.println("== 프로그램 끝 ==");
+	}
 }
